357585c91b0e7f68b55f70ef45c9e12b246080f8
Miguel Ángel <miguel.angel.gargo@gmail.com>
2016-02-03 18:52:19 +0100

    Spring security configuration and test user added

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,22 @@
+import crudexample.g3.Role
+import crudexample.g3.User
+import crudexample.g3.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        Role adminRole = new Role('ADMIN').save()
+        User testUser = new User('admin', 'admin').save()
+        UserRole.create testUser, adminRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert User.count() == 1
+        assert Role.count() == 1
+        assert UserRole.count() == 1
     }
     def destroy = {
     }
